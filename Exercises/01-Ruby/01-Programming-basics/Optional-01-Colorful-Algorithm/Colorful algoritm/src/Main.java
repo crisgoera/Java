641d1e3443964fee61169745ba40a66244e8c39d
Cristian Goenaga <crisgoera@gmail.com>
2023-11-26 09:54:06 +0100

    Input and digit separation methods implemented

--- Exercises/01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/Colorful algoritm/src/Main.java
+++ Exercises/01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/Colorful algoritm/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
+
+    public static char[] inputNumber() {
+        System.out.println("Introduce you number: \n");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        char[] digitArray = input.toCharArray();
+        return digitArray;
+    }
+
+    public static ArrayList<Integer> separateDigits(char[] input) {
+        ArrayList<Integer> resultArray = new ArrayList<Integer>();
+
+        for (int i=0; i<input.length; i++) {
+            resultArray.add(Character.getNumericValue(input[i]));
+        }
+
+        resultArray.add(resultArray.get(0)*resultArray.get(1));
+        resultArray.add(resultArray.get(1)*resultArray.get(2));
+        return resultArray;
+    }
+
+    public static boolean colorful(ArrayList<Integer> input) {
+        for (int i=0; i<input.size(); i++) {
+            if (input.get(i)==)
+        }
+    }
+
 }

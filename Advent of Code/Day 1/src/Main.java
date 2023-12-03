76d1ebc442d3328416048c0f066836c667a82732
Cristian Goenaga <crisgoera@gmail.com>
2023-12-03 17:31:45 +0100

    Advent of Code Day 1

---
+++ Advent of Code/Day 1/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.*;
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "https://adventofcode.com/2023/day/1/input";
+        System.out.println(Jsoup.connect(url));
+    }
+
+//    Target website scraper. "https://adventofcode.com/2023/day/1/input"
+
+    public static ArrayList<String> scraper(String url) {
+        ArrayList<String> resultList = new ArrayList<String >();
+
+        return resultList;
+    }
+}

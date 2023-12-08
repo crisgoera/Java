961574e04a0a60ce8764dbc22edabb7a7ec0c6ff
Cristian Goenaga <crisgoera@gmail.com>
2023-12-08 18:50:52 +0100

    Comments added

--- Advent of Code/Day 3/src/Row.java
+++ Advent of Code/Day 3/src/Row.java
@@ -3,10 +3,12 @@ import java.util.ArrayList;
 public class Row {
     String rowData;
 
+//    Generates Object Row with the string data from the input text.
     public void generate(String data) {
         rowData = data;
     }
 
+//    Checks if a character is a special character.
     public boolean checkSymbol(char chr){
         String symbolPool = "123456789.";
         String character = Character.toString(chr);
@@ -14,6 +16,7 @@ public class Row {
         return !symbolPool.contains(character);
     }
 
+//    Obtains the list in indexes where the special characters are located
     public ArrayList<Integer> getIndexes(){
         ArrayList<Integer> ret = new ArrayList<Integer>();
         char[] charArray = rowData.toCharArray();
